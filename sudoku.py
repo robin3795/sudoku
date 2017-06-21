1e5d70dae8bc599df1053240fe57abc12067e70e
Robin <robinli@live.ca>
2017-06-20 21:52:57 -0400

    Ver 2.0.3 Remove check group size

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,18 @@
 #!\Python33 python
 #!\Python 3.6.0
-"""Sudoku.py Ver 2.0.1
+"""Sudoku.py Ver 2.0.3
+# Remove check group size
 Developed by Robin Li robinli@live.ca
 """
 
 def main():
     global groups
     dataList = []
+    groups = groupGenerator()
     dataList = readFile("board")
     messageWelcome = "Welcome to play the Sudoku Games !"
     messageDone = "Congratulation! This Sudoku is solved !"
     messagePlus = "Oh.. Hard game, and we need more smarter algorithms!"
-    groups = groupGenerator()
     display(messageWelcome,dataList)
     dataList = loop(dataList)
     if (doneLoop(dataList)):
@@ -28,17 +29,17 @@ def display(message,dataList):
     print ("       " + message + " \n")
     print (displayIndent + "+-------+-------+-------+")
     for i in range(0,9):
-        print(displayIndent + "| ", end='')
+        print(displayIndent + "| ", end = '')
         for j in range(0,9):
             v = dataList[9*i+j]
             if ( v != 0 and len(v) == 1):
-                print(v+" ", end='')
+                print(v + " ", end = '')
             else:
-                print(". ", end='')
+                print(". ", end = '')
             if (j == 2 or j == 5):
-                print("| ", end='')
+                print("| ", end = '')
         print("|")
-        if (i==2 or i==5):
+        if (i == 2 or i == 5):
                 print (displayIndent + "+-------+-------+-------+")
     print (displayIndent + "+-------+-------+-------+\n\n")
 
@@ -51,21 +52,21 @@ def readFile(infile):
     for i in range(0,12):
         if (i % 4 != 0):
             line = content[i]
-            tempList = line.split(" ")
+            lineList = line.split(" ")
             for j in range(0,11):
-                if (j !=3 and j!=7):
-                    if (tempList[j] == "."):
+                if (j != 3 and j != 7):
+                    if (lineList[j] == "."):
                         data.append("123456789")
                     else:
-                        data.append(tempList[j])
+                        data.append(lineList[j])
     return data
 
 
 def nineNumberList():
-    tempList = []
+    numbersList = []
     for i in range(1,10):
-        tempList.append(str(i))
-    return tempList
+        numbersList.append(str(i))
+    return numbersList
 
 
 def groupGenerator():
@@ -91,42 +92,39 @@ def groupGenerator():
     return totalGroups
 
 def excludedFunction(group,data):
-    if (len(group) == 9):
-        doneList = []
-        for item in group:
-            if (len(data[item]) ==1 ):
-                doneList.append(data[item])
-        for item in group:
-            if (len(data[item]) !=1 ):
-                for doneItem in doneList:
-                    data[item] = data[item].replace(doneItem,"")
+    doneList = []
+    for i in group:
+        if (len(data[i]) == 1):
+            doneList.append(data[i])
+    for i in group:
+        if (len(data[i]) != 1):
+            for doneItem in doneList:
+                data[i] = data[i].replace(doneItem,"")
     return data
 
 
 def uniqueFunction(group,data):
-    if (len(group) == 9):
-        notDoneList = nineNumberList()
-        notDoneString = ""
-        for item in group:
-            if (len(data[item]) == 1):
-                if (data[item] in notDoneList):
-                    notDoneList.remove(data[item])
-            else:
-                notDoneString = notDoneString + data[item]
-        for numString in notDoneList:
-            if (notDoneString.count(numString) == 1):
-                for item in group:
-                    if (data[item].count(numString)==1):
-                        data[item]=numString                
+    notDoneList = nineNumberList()
+    notDoneString = ""
+    for i in group:
+        if (len(data[i]) == 1):
+            if (data[i] in notDoneList):
+                notDoneList.remove(data[i])
+        else:
+            notDoneString = notDoneString + data[i]
+    for numString in notDoneList:
+        if (notDoneString.count(numString) == 1):
+            for i in group:
+                if (data[i].count(numString) == 1):
+                    data[i] = numString                
     return data
 
 
 def groupDone(group,data):
-    if (len(group) == 9):
-        doneFlag = True
-        for item in group:
-            if (len(data[item]) != 1 ):
-                doneFlag = False                
+    doneFlag = True
+    for i in group:
+        if (len(data[i]) != 1):
+            doneFlag = False                
     return doneFlag
 
 
@@ -134,6 +132,7 @@ def excludedLoop(data):
     for i in range(0,27):
         data = excludedFunction(groups[i],data)
     return data
+
 
 def uniqueLoop(data):
     for i in range(0,27):            
@@ -158,8 +157,9 @@ def loop(data):
         i = i + 1
     return data
 
+
 def loopPlus(data):
-    stopLoop = False
+    okFlag = False
     for i in range(0,27):
         group = groups[i]
         for item in group:
@@ -169,39 +169,39 @@ def loopPlus(data):
                 tempData=loop(tempData)
                 if okLoop(tempData):
                     data = tempData
-                    stopLoop = True
+                    okFlag = True
                     break
                 else:
                     tempData[item] = data[item][1]
                     tempData=loop(tempData)
                     if okLoop(tempData):
                         data = tempData
-                        stopLoop = True
+                        okFlag = True
                         break
-        if (stopLoop):
+        if (okFlag):
             break
     return data
 
 def copyList(inList):
     outList = []
-    for item in inList:
-        outList.append(item)
+    for i in inList:
+        outList.append(i)
     return outList
     
 
 def groupOK(group,data):
-    if (len(group) == 9):
-        okFlag = True
-        tempList = nineNumberList()
-        for item in group:
-            if (len(data[item]) != 1 ):
-                okFlag = False
-            else:
-                if (data[item] in tempList):
-                    tempList.remove(data[item])
-        if (len(tempList) != 0 ):
+    okFlag = True
+    numberList = nineNumberList()
+    for i in group:
+        if (len(data[i]) != 1):
             okFlag = False
+        else:
+            if (data[i] in numberList):
+                numberList.remove(data[i])
+    if (len(numberList) != 0):
+        okFlag = False
     return okFlag
+
 
 def okLoop(data):
     okFlag = True
